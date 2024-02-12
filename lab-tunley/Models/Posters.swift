04aed72acce6cb8ec6baf786dbd7eef5dd1cc3be
littleximena <xfern004@fiu.edu>
2024-02-12 18:58:57 -0500

    commit

---
+++ lab-tunley/Models/Posters.swift
@@ -0,0 +1,8 @@
+//
+//  Posters.swift
+//  lab-tunley
+//
+//  Created by Ximena on 2/12/24.
+//
+
+import Foundation
---
+++ lab-tunley/PosterCell.swift
@@ -0,0 +1,12 @@
+//
+//  PosterCell.swift
+//  lab-tunley
+//
+//  Created by Ximena on 2/12/24.
+//
+
+import UIKit
+
+class PosterCell: UICollectionViewCell {
+    
+}
